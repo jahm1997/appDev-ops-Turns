e841405b751c67678001e8288790971b944a0b29
Mauricio <Mauricio@EDOM>
2024-11-22 11:53:39 -0500

    Hice una modificación para probar github

--- src/main/java/com/unicartagena/turnos/Turnos.java
+++ src/main/java/com/unicartagena/turnos/Turnos.java
@@ -8,14 +8,16 @@ import com.unicartagena.turnos.view.VistaInicio;
 
 /**
  *
- * @author jahm1
+ * @author Edom1020
  */
 public class Turnos {
+    //Aquí inicializaremos la aplicación//
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new VistaInicio().setVisible(true);
             }
         });
+        
     }
 }
